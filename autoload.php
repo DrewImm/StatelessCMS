cd1a9938abab7151349075d6969a61b7062d76da
Drew Immerman <dimmerman@localhost.localdomain>
2017-11-01 08:01:31 -0400

    Created autoloader

---
+++ autoload.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * autoload.php
+ * 
+ * @package StatelessCMS
+ * @version 0.0.1
+ */
+
+/**
+ * @brief Autoload module files
+ * @param string $load Class name to load
+ */
+spl_autoload_register(function($load) {
+    if (strpos($load, "Stateless\\") !== false) {
+        $load = str_replace("\\", "/", $load);
+        $load = str_replace("Stateless", "lib", $load);
+        include_once(__DIR__ . "/" . $load . ".php");
+    }
+});
