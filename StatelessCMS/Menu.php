99037a88998eb0bbbd60c194022500d31c65f26a
Drew Immerman <drewimmerman@gmail.com>
2018-01-17 18:20:04 -0500

    Checked for falsiness of arguments and added defaults

--- StatelessCMS/Menu.php
+++ StatelessCMS/Menu.php
@@ -3,26 +3,41 @@
 namespace Stateless;
 
 /**
- * @brief A Menu contains a list of MenuItem objects and build the markup
+ * A Menu contains a list of MenuItem objects and build the markup
  */
 class Menu {
-    public $items = array(); /**< Array of MenuItem objects */
-    public $attributes = array(); /**< Key/value pairs to attach to the tag */
+    /** Array of MenuItem objects */
+    public $items = array();
+
+    /** Key/value pairs to attach to the tag */
+    public $attributes = array();
 
     /**
-     * @brief Create a new Menu
+     * Create a new Menu
+     * 
      * @param array $items Array of MenuItem or MenuIcon objects
      * @param array $attributes Key/value pairs to attach to the tag
      */
     public function __construct($items = [], $attributes = []) {
-        $this->items = $items;
-        $this->attributes = $attributes;
+        if (!empty($items)) {
+            $this->items = $items;
+        }
+        
+        if (!empty($attributes)) {
+            $this->attributes = $attributes;
+        }
     }
 
     /**
-     * @brief Output the menu and it's items to the current output buffer
+     * Output the menu and it's items to the current output buffer
      */
     public function show() {
+
+        // Defaults
+        if (!$this->attributes || !array_key_exists("class", $this->attributes)) {
+            $this->attributes["class"] = "nav";
+        }
+
         echo "<ul";
 
         // Output attributes
@@ -41,4 +56,4 @@ class Menu {
         echo "</ul>";
     }
 
-}
+};
