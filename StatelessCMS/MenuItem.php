bd3d0160cf58648a5daa481f86387a7e97aafa77
Drew Immerman <drewimmerman@gmail.com>
2018-01-17 18:24:48 -0500

    Refactored and cleaned up

--- StatelessCMS/MenuItem.php
+++ StatelessCMS/MenuItem.php
@@ -3,141 +3,179 @@
 namespace Stateless;
 
 /**
- * @brief A single item in a Menu
+ * A single item in a Menu
  */
 class MenuItem {
-     public $label; /**< Human-readable text to display */
-     public $href; /**< Path href the menu item should execute */
-     public $tooltip; /**< Optional tooltip to attach to this menu item */
-     public $tooltipLocation; /**< Optional tooltip location */
-     public $attributes; /**< Array of additional attributes */
-     public $id; /**< Optional id attribute for the item */
-     public $class; /**< Optional CSS class to attach to this menu item */
-     public $linkClass = false;
+
+    /** Human-readable text to display */
+    public $label;
+
+    /** Path href the menu item should execute */
+    public $href;
+
+    /** Optional tooltip to attach to this menu item */
+    public $tooltip;
+
+    /** Optional tooltip location */
+    public $tooltipLocation = "top";
+
+    /** Array of additional attributes */
+    public $attributes;
+
+    /** Array of additional attributes for the link */
+    public $linkAttributes;
+
+    /** Menu object as a nested submenu */
+    public $submenu;
 
     /**
-     * @brief Create a new menu item
-     * @param mixed $label Human-readable text to display or Array of key
-     *  value pairs
-     * @param string $href Path href the menu item should execute
-     * @param mixed $tooltip Tooltip string to use, or false for none
-     * @param string $tooltipLocation Tooltip location.  Default is "bottom"
-     * @param array $attributes Array of additional attributes
-     * @param string $id Optional id attribute for the item
-     * @param string $class Optional CSS class to attach to this menu item
+     * Create a new menu item
+     * 
+     * @param string $label Menu label text/html
+     * @param string $href Menu link path
+     * @param array $data (Optional) Data to pass to the menu item
     */
-    public function __construct(
-        $label,
-        $href,
-        $tooltip = false,
-        $tooltipLocation = "bottom",
-        $attributes = array(),
-        $id = false,
-        $class = false
-    ) {
-        
-        // Check if first parameter is array
+    public function __construct($label = false, $href = false, $data = []) {
+
+        // Check if an array was passed as the label
         if (is_array($label)) {
             $data = $label;
-
-            if (array_key_exists("label", $data)) {
-                $label = $data["label"];
-            }
-
-            if (array_key_exists("href", $data)) {
-                $href = $data["href"];
-            }
-
-            if (array_key_exists("tooltip", $data)) {
-                $tooltip = $data["tooltip"];
-            }
-
-            if (array_key_exists("tooltip_location", $data)) {
-                $tooltipLocation = $data["tooltip_location"];
-            }
-
-            if (array_key_exists("attributes", $data)) {
-                $attributes = $data["attributes"];
-            }
-
-            if (array_key_exists("id", $data)) {
-                $id = $data["id"];
-            }
-
-            if (array_key_exists("class", $data)) {
-                $class = $data["class"];
-            }
-
-            if (array_key_exists("link_class", $data)) {
-                $linkClass = $data["link_class"];
-            }
-            
+            $label = false;
         }
 
         $this->label = $label;
         $this->href = $href;
-        $this->id = $id;
-        $this->class = $class;
-        $this->tooltip = $tooltip;
-        $this->tooltipLocation = $tooltipLocation;
-        $this->attributes = $attributes;
+
+        // Check if $data is array
+        if ($data && is_array($data)) {
+
+            // Label
+            if (array_key_exists("label", $data)) {
+                $this->label = $data["label"];
+            }
+
+            // Href
+            if (array_key_exists("href", $data)) {
+                $this->href = $data["href"];
+            }
+
+            // Tooltip
+            if (array_key_exists("tooltip", $data)) {
+                $this->tooltip = $data["tooltip"];
+            }
+
+            // Tooltip location
+            if (array_key_exists("tooltip_location", $data)) {
+                $this->tooltipLocation = $data["tooltip_location"];
+            }
+
+            // Attributes
+            if (array_key_exists("attributes", $data)) {
+                $this->attributes = $data["attributes"];
+            }
+
+            // Link attributes
+            if (array_key_exists("link_attributes", $data)) {
+                $this->linkAttributes = $data["link_attributes"];
+            }
+
+            // Nested submenu
+            if (array_key_exists("submenu", $data)) {
+                $this->submenu = $data["submenu"];
+            }
+
+        }
+
     }
 
     /**
-     * @brief Output the menu item markup to the current output buffer
+     * Output the menu item markup to the current output buffer
      */
     public function show() {
-        if ($this->label instanceof Menu) {
-            $this->label->show();
+
+        // Defaults
+
+        // Class
+        if (!is_array($this->attributes) ||
+            !array_key_exists("class", $this->attributes)) {
+        
+            // Set default class
+            $this->attributes["class"] = "nav-item";
         }
-        else {
-            echo "<li";
-    
-            // Id
-            if (!empty($this->id)) {
-                echo " id=\"" . $this->id . "\"";
-            }
-    
-            // Class
-            if (!empty($this->class)) {
-                echo " class=\"" . $this->class . "\"";
-            }
-    
-            // Tooltip
-            if (!empty($this->tooltip)) {
-                echo 
-                    " data-toggle=\"tooltip\"" .
-                    " data-placement=\"" . $this->tooltipLocation . "\"" .
-                    " title=\"" . $this->tooltip . "\""
-                ;
-            }
-    
-            //  Attributes
-            if (!empty($this->attributes)) {
-                foreach ($this->attributes as $key => $value) {
-                    echo " " . $key . "=\"" . $value . "\"";
-                }
-            }
-    
-            echo ">";
-    
-            // Active
-            if ($this->href === Request::getPath() ||
-                (
-                    strpos(Request::getPath(), $this->href) !== false &&
-                    $this->href !== "/"
-                )) {
-                $this->linkClass .= " active";
-            }
-    
-            // Link
-            echo "<a href=\"" . $this->href . "\" class=\"" . $this->linkClass . "\">";
-    
-            // Label
-            echo $this->label;
-    
-            // Closeup
-            echo "</a></li>";
+
+        // Link class
+        if (!is_array($this->linkAttributes) ||
+            !array_key_exists("class", $this->linkAttributes)) {
+        
+            // Set default class
+            $this->linkAttributes["class"] = "nav-link";
         }
+
+        // Output list item
+        echo "<li";
+
+        // Tooltip
+        if (!empty($this->tooltip)) {
+            echo 
+                " data-toggle=\"tooltip\"" .
+                " data-placement=\"" . $this->tooltipLocation . "\"" .
+                " title=\"" . $this->tooltip . "\""
+            ;
+        }
+
+        //  Attributes
+        if (!empty($this->attributes)) {
+            foreach ($this->attributes as $key => $value) {
+                echo " " . $key . "=\"" . $value . "\"";
+            }
+        }
+
+        echo ">";
+
+        // Check if this link matches the request path
+        $activeClass = false;
+        if ($this->href === Request::getPath() ||
+            (
+                strpos(Request::getPath(), $this->href) !== false &&
+                $this->href !== "/"
+            )) {
+                
+            if (!$this->linkAttributes ||
+                !array_key_exists("class", $this->attributes)) {
+
+                // Create class attribute
+                $this->linkAttributes["class"] = "active";
+            }
+            else {
+
+                // Update class attribute
+                $this->linkAttributes["class"] .= " active";
+            }
+        }
+
+        // Output link
+        echo "<a href=\"" . $this->href . "\"";
+        
+        foreach ($this->linkAttributes as $key => $value) {
+            echo " " . $key . "=\"" . $value . "\"";
+        }
+        
+        echo ">";
+
+        // Label
+        echo $this->label;
+
+        // Close link
+        echo "</a>";
+
+        // Check for nested menus
+        if ($this->submenu && $this->submenu instanceof Menu) {
+            $this->submenu->show();
+        }
+
+        // Closeup
+        echo "</li>";
+
     }
-}
+
+};
